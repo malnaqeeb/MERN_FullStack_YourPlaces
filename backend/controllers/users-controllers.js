0b9a2990e34111a392d8c40e35d0ca03f265fa0a
Serafettin Bayram <sbayram2016@gmail.com>
2020-03-26 02:18:39 +0100

    sort users done

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 
 const User = require('../model/user');
 const HttpError = require('../model/http-error');
@@ -8,8 +8,8 @@ const JWT_KEY = process.env.JWT_KEY;
 
 const getUserFriend = async (req, res, next) => {
   const user = await User.findById(req.userData.userId)
-    .populate({path: 'friends', model: User})
-    .populate({path: 'friendRequests.user', model: User});
+    .populate({ path: 'friends', model: User })
+    .populate({ path: 'friendRequests.user', model: User });
 
   res.status(201).json({
     userId: req.userData.userId,
@@ -18,56 +18,63 @@ const getUserFriend = async (req, res, next) => {
     name: user.name,
     friends: !user.friends
       ? []
-      : user.friends
-        .toObject()
-        .map(friend => ({
+      : user.friends.toObject().map(friend => ({
           id: friend._id,
           name: friend.name,
           email: friend.email,
-          image: friend.image
+          image: friend.image,
         })),
     friendRequests: !user.friendRequests
       ? []
-      : user.friendRequests
-        .toObject()
-        .map(request => ({
+      : user.friendRequests.toObject().map(request => ({
           ...request,
           user: {
             id: request.user._id,
             email: request.user.email,
             image: request.user.image,
-            name: request.user.name
-          }
-        }))
+            name: request.user.name,
+          },
+        })),
   });
 };
 
 const getUsers = async (req, res, next) => {
+  const sortBy = req.query.sortBy || 'name';
+
   let users;
 
   try {
-    users = await User.find({}, '-password');
+    users = await User.find({}, '-password').sort(sortBy);
+    console.log(users);
   } catch (error) {
-    return next(new HttpError('Fetching users failed, please try again later.', 500));
+    return next(
+      new HttpError('Fetching users failed, please try again later.', 500),
+    );
   }
-  res.status(200).json({users: users.map(user => user.toObject({getters: true}))});
+  res
+    .status(200)
+    .json({ users: users.map(user => user.toObject({ getters: true })) });
 };
 
 const signup = async (req, res, next) => {
   const error = validationResult(req);
 
   if (!error.isEmpty()) {
-    return next(new Error('Invalid input passed, please check your data.', 422));
+    return next(
+      new Error('Invalid input passed, please check your data.', 422),
+    );
   }
 
-  const {name, email, password} = req.body;
+  const { name, email, password } = req.body;
 
   let createdUser;
   try {
-    const existingUser = await User.findOne({email: email});
+    const existingUser = await User.findOne({ email: email });
 
     if (existingUser) {
-      return next(new HttpError('User exists already, please login instead.', 422));
+      return next(
+        new HttpError('User exists already, please login instead.', 422),
+      );
     }
 
     createdUser = new User({
@@ -76,26 +83,38 @@ const signup = async (req, res, next) => {
       image: req.file.url,
       password,
       social: {},
-      places: []
+      places: [],
+      created_at: new Date(),
     });
 
     await createdUser.save();
   } catch (error) {
-    return next(new HttpError('Signin up  failed, please try again later.', 500));
+    console.log(error);
+    return next(
+      new HttpError('Signin up  failed, please try again later.', 500),
+    );
   }
   let token;
   try {
-    token = jwt.sign({userId: createdUser.id, email: createdUser.email, token}, JWT_KEY, {
-      expiresIn: '1h'
-    });
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email, token },
+      JWT_KEY,
+      {
+        expiresIn: '1h',
+      },
+    );
   } catch (error) {
-    return next(new HttpError('Signing up failed, please try again later', 500));
+    return next(
+      new HttpError('Signing up failed, please try again later', 500),
+    );
   }
-  res.status(201).json({userId: createdUser.id, email: createdUser.email, token});
+  res
+    .status(201)
+    .json({ userId: createdUser.id, email: createdUser.email, token });
 };
 
 const login = async (req, res, next) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
 
   let existingUser;
   try {
@@ -106,45 +125,58 @@ const login = async (req, res, next) => {
 
   let token;
   try {
-    token = jwt.sign({userId: existingUser.id, email: existingUser.email, token}, JWT_KEY, {
-      expiresIn: '1h'
-    });
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email, token },
+      JWT_KEY,
+      {
+        expiresIn: '1h',
+      },
+    );
   } catch (error) {
-    return next(new HttpError('Logging in failed, please try agein later', 500));
+    return next(
+      new HttpError('Logging in failed, please try agein later', 500),
+    );
   }
-  res.status(201).json({userId: existingUser.id, email: existingUser.email, token});
+  res
+    .status(201)
+    .json({ userId: existingUser.id, email: existingUser.email, token });
 };
 
 const signJwt = async (req, res, next) => {
   let token;
   try {
-    token = jwt.sign({userId: req.user._id, email: req.user.email}, JWT_KEY, {
-      expiresIn: '1h'
+    token = jwt.sign({ userId: req.user._id, email: req.user.email }, JWT_KEY, {
+      expiresIn: '1h',
     });
   } catch (error) {
-    return next(new HttpError('Logging in failed, please try again later', 500));
+    return next(
+      new HttpError('Logging in failed, please try again later', 500),
+    );
   }
-  res.status(201).redirect(`${process.env.AUTH_REDIRECT_PATH}/social?userId=${req.user._id}&token=${token}`);
+  res
+    .status(201)
+    .redirect(
+      `${process.env.AUTH_REDIRECT_PATH}/social?userId=${req.user._id}&token=${token}`,
+    );
 };
-
 
 const getUser = async (req, res, next) => {
   let user;
   try {
     user = await User.findById(req.params.userId, 'name image');
   } catch (error) {
-    return next(new HttpError('Failed to get the user, please try again later.', 500));
+    return next(
+      new HttpError('Failed to get the user, please try again later.', 500),
+    );
   }
-  res.status(201).json({user});
+  res.status(201).json({ user });
 };
 
 const updateUser = async (req, res, next) => {
   let user;
 
   if (req.params.userId !== req.userData.userId) {
-    return next(
-      new HttpError('Not authorized.', 401)
-    );
+    return next(new HttpError('Not authorized.', 401));
   }
 
   try {
@@ -154,12 +186,18 @@ const updateUser = async (req, res, next) => {
     await user.save();
   } catch {
     return next(
-      new HttpError('Updating user failed, please try again later.', 500)
+      new HttpError('Updating user failed, please try again later.', 500),
     );
   }
-  res
-    .status(200)
-    .json({user: {name: user.name, image: user.image}});
+  res.status(200).json({ user: { name: user.name, image: user.image } });
 };
 
-module.exports = {getUsers, signup, login, getUser, updateUser, signJwt, getUserFriend};
+module.exports = {
+  getUsers,
+  signup,
+  login,
+  getUser,
+  updateUser,
+  signJwt,
+  getUserFriend,
+};
--- backend/model/place.js
+++ backend/model/place.js
@@ -6,46 +6,47 @@ const placeSchema = new Schema({
   title: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   description: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   image: {
     id: {
       type: String,
-      required: true
+      required: true,
     },
     imageUrl: {
       type: String,
       required: true,
-      trim: true
-    }
+      trim: true,
+    },
   },
   address: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   location: {
     lng: {
       type: Number,
-      required: true
+      required: true,
     },
     lat: {
       type: Number,
-      required: true
-    }
+      required: true,
+    },
   },
   creator: {
     type: mongoose.Types.ObjectId,
     required: true,
-    ref: 'User'
+    ref: 'User',
   },
-  likes: {type: Array},
-  disLike: {type: Array}
+  likes: { type: Array },
+  disLike: { type: Array },
+  created_at: { type: Date, required: true, default: Date.now },
 });
 
 module.exports = mongoose.model('Place', placeSchema);
