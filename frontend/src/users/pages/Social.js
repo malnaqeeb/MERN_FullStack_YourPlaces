51c39baacf8dd836b094b994ebefd15113e1298e
Dua Hasan <doahajer@gmail.com>
2020-04-01 15:28:05 +0200

    social page code updating

--- frontend/src/users/pages/Social.js
+++ frontend/src/users/pages/Social.js
@@ -3,12 +3,10 @@ import { AuthContext } from "../../shared/context/auth-context";
 
 export default function Social() {
   const auth = useContext(AuthContext);
-  const socialLog = async () => {
-    try {
-      const token = new URL(window.location).searchParams.get("token");
-      const uId = new URL(window.location).searchParams.get("userId");
-      auth.login(uId, token);
-    } catch {}
+  const socialLog = () => {
+    const token = new URL(window.location).searchParams.get("token");
+    const uId = new URL(window.location).searchParams.get("userId");
+    auth.login(uId, token);
   };
   useEffect(() => {
     socialLog();
