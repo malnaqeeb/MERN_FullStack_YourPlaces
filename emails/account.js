a65bceb72f3eefae9e789bdc1d9e4ca6df353f27
Sila <basaksilakaymak@gmail.com>
2020-04-23 18:43:06 +0200

    notifications improved

--- emails/account.js
+++ emails/account.js
@@ -1,16 +1,75 @@
-const sendGridApiKey = process.env.SEND_GRID_API_KEY
+const sendGridApiKey = "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";
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(sendGridApiKey);
-const HttpError = require('../model/http-error')
+const HttpError = require("../model/http-error");
+
+const template = (name, firstText, linkPath, secondText, linkText) => {
+  return `<html>
+  <head>
+    <title>Forgot Password</title>
+    <style>
+      div{
+        width:99.8%;
+        margin:auto;
+        text-align:center;
+        border:1px solid #FF0055;
+        font-family: Arial;
+      }
+      strong{
+        color:yellow;
+        font-size:2rem;
+      }
+      .link-cl{
+        text-decoration:none;
+        color: yellow;
+        border: 2px solid white;
+        display:block;
+        text-align:center;
+        margin:auto;
+        padding:0.25rem;
+        min-width: 3rem;
+        max-width:40%;
+        min-height:1rem;
+      }
+      .link-cl:hover{
+        background:yellow;
+        font-weight:bold;
+        color:#FF0055;
+        border-color: #FF0055;
+      }
+      h2, p{
+        color:#FF0055;
+      }
+    </style>
+  </head>
+  <body>
+    <div>
+      <strong>Hello ${name}</strong>
+      <p>${firstText}</p>
+      <a href=${linkPath} class="link-cl">${linkText}</a>
+      <h2>YourPlaces App</h2>
+      <p><em>${secondText}</em></p>
+    </div>
+  </body>
+</html>`;
+};
 
 const forgetPasswordEmail = async (name, email, link) => {
   const mailOptions = {
     to: email,
     from: "bsilakaymak@gmail.com",
+    name: "YourPlaces",
     subject: "Password change request",
-    text: `Hi ${name} \n 
+    html: template(
+      name,
+      `Please click on the following link to reset your password.`,
+      link,
+      ` If you did not request this, please ignore this email and your password will remain unchanged.`,
+      `Click to Change Your Password`
+    ),
+    text: `Hello ${name} \n 
        Please click on the following link ${link} to reset your password. \n\n 
-       If you did not request this, please ignore this email and your password will remain unchanged.\n`
+       If you did not request this, please ignore this email and your password will remain unchanged.\n`,
   };
   try {
     await sgMail.send(mailOptions);
@@ -24,8 +83,15 @@ const resetPasswordEmail = async (name, email) => {
     to: email,
     from: "bsilakaymak@gmail.com",
     subject: "Your password has been changed",
-    text: `Hi ${name} \n 
-          This is a confirmation that the password for your account ${email} has just been changed.\n`
+    html: template(
+      name,
+      `This is a confirmation that the password for your account ${email} has just been changed.`,
+      `http://placesharer.herokuapp.com`,
+      ` `,
+      `Go to App`
+    ),
+    text: `Hello ${name} \n 
+          This is a confirmation that the password for your account ${email} has just been changed.\n`,
   };
   try {
     await sgMail.send(mailOptions);
@@ -34,15 +100,20 @@ const resetPasswordEmail = async (name, email) => {
   }
 };
 
-
-
 const friendAddedNotification = async (name, person, email) => {
   const mailOptions = {
     to: email,
     from: "yourplaces.hyf@gmail.com",
     subject: "You have a new friend request",
-    text: `Hi ${name} \n 
-           ${person} wants to be your friend \n`
+    text: `Hello ${name} \n 
+           ${person} wants to be your friend \n`,
+    html: template(
+      name,
+      `${person} wants to be your friend`,
+      `http://placesharer.herokuapp.com`,
+      `If you do not want to receive e-mail notifications, you can disable it on your profile page.`,
+      `Go to App`
+    ),
   };
   try {
     await sgMail.send(mailOptions);
@@ -56,8 +127,15 @@ const friendAcceptedNotification = async (name, person, email) => {
     to: email,
     from: "yourplaces.hyf@gmail.com",
     subject: "Your friend request is accepted",
-    text: `Hi ${name} \n 
-          You are now friends with ${person}\n`
+    html: template(
+      name,
+      `You are now friends with ${person}`,
+      `http://placesharer.herokuapp.com`,
+      `If you do not want to receive e-mail notifications, you can disable it on your profile page.`,
+      `Go to App`
+    ),
+    text: `Hello ${name} \n 
+          You are now friends with ${person}\n`,
   };
   try {
     await sgMail.send(mailOptions);
@@ -71,9 +149,16 @@ const likeNotification = async (name, person, place, email) => {
     to: email,
     from: "bsilakaymak@gmail.com",
     subject: "Someone liked your place",
-    text: `   Hi ${name}, 
+    html: template(
+      name,
+      `${person} liked your place ${place}`,
+      `http://placesharer.herokuapp.com`,
+      `If you do not want to receive e-mail notifications, you can disable it on your profile page.`,
+      `Go to App`
+    ),
+    text: `   Hello ${name}, 
     ${person} liked your place ${place}
-    If you no longer want to receive notifications, you can turn them off in your profile page\n`
+    If you do not want to receive e-mail notifications, you can disable it on your profile page.\n`,
   };
   try {
     await sgMail.send(mailOptions);
@@ -82,11 +167,35 @@ const likeNotification = async (name, person, place, email) => {
   }
 };
 
+const friendSharedPlace = async (friend, place, emails) => {
+  const mailOptions = {
+    to: emails,
+    from: "bsilakaymak@gmail.com",
+    name: "YourPlaces",
+    subject: "Your friend shared a new place",
+    html: template(
+      `!`,
+      `Your friend ${friend} shared a new place : ${place}`,
+      `http://placesharer.herokuapp.com`,
+      `If you do not want to receive e-mail notifications, you can disable it on your profile page.`,
+      `Go to App`
+    ),
+    text: `Hello! \n 
+       Your friend ${friend} shared a new place : ${place} \n\n 
+       If you do not want to receive e-mail notifications, you can disable it on your profile page.\n`,
+  };
+  try {
+    await sgMail.send(mailOptions);
+  } catch (error) {
+    return new HttpError(error.message, 500);
+  }
+};
 
 module.exports = {
   forgetPasswordEmail,
   resetPasswordEmail,
   friendAddedNotification,
   friendAcceptedNotification,
-  likeNotification
+  likeNotification,
+  friendSharedPlace
 };
--- register-confirmation/mail-generator.js
+++ register-confirmation/mail-generator.js
@@ -1,41 +1,108 @@
-const sgMail = require('@sendgrid/mail');
-const HttpError = require('../model/http-error')
-const sendGridApiKey = process.env.SEND_GRID_API_KEY
+const sgMail = require("@sendgrid/mail");
+const HttpError = require("../model/http-error");
+const sendGridApiKey = process.env.SEND_GRID_API_KEY;
 
 sgMail.setApiKey(sendGridApiKey);
 
+const template = (name, firstText, linkPath, secondText, linkText) => {
+  return `<html>
+  <head>
+    <title>Forgot Password</title>
+    <style>
+      div{
+        width:99.8%;
+        margin:auto;
+        text-align:center;
+        border:1px solid #FF0055;
+        font-family: Arial;
+      }
+      strong{
+        color:yellow;
+        font-size:2rem;
+      }
+      .link-cl{
+        text-decoration:none;
+        color: yellow;
+        border: 2px solid white;
+        display:block;
+        text-align:center;
+        margin:auto;
+        padding:0.25rem;
+        min-width: 3rem;
+        max-width:30%;
+        min-height:1rem;
+      }
+      .link-cl:hover{
+        background:yellow;
+        font-weight:bold;
+        color:#FF0055;
+        border-color: #FF0055;
+      }
+      h2, p{
+        color:#FF0055;
+      }
+    </style>
+  </head>
+  <body>
+    <div>
+      <strong>Hello ${name}</strong>
+      <p>${firstText}</p>
+      <a href=${linkPath} class="link-cl">${linkText}</a>
+      <h2>YourPlaces App</h2>
+      <p>${secondText}</p>
+    </div>
+  </body>
+</html>`;
+};
+
 const accountVerifyEmail = async (name, email, link) => {
-  const mailGenerator  = {
+  const mailGenerator = {
     to: email,
     from: "yourplaces.hyf@gmail.com",
     subject: "Confirm your Account",
-    text: `Hi ${name} \n 
+    html: template(
+      name,
+      `Congratulations! You're almost set to start using YourPlaces
+    Just click the link below to validate your email address.`,
+      link,
+      `Verifying your email ensures that you can access and manage your account, and receive critical notifications.
+    Note: You must perform this validation within one hour to keep your new account enabled, otherwise you need to sign up once again`,
+      `Confirm`
+    ),
+    text: `Hello ${name} \n 
       Congratulations! You're almost set to start using YourPlaces.\n
       Just click the link below to validate your email address. \n\n
         ${link} \n\n
       Verifying your email ensures that you can access and manage your account, and receive critical notifications.\n\n
-      Note: You must perform this validation within one hour to keep your new account enabled, otherwise you need to sign up once again\n`
+      Note: You must perform this validation within one hour to keep your new account enabled, otherwise you need to sign up once again\n`,
   };
   try {
-    await sgMail.send(mailGenerator );
+    await sgMail.send(mailGenerator);
   } catch (error) {
     return new HttpError(error.message, 500);
   }
 };
 const accountActivatedEmail = async (name, email) => {
-  const mailGenerator  = {
+  const mailGenerator = {
     to: email,
     from: "yourplaces.hyf@gmail.com",
     subject: "Confirmation",
+    html: template(
+      name,
+      `Your account ${email} has been activated successfully.You may now log in and begin using it.`,
+      `https://placesharer.herokuapp.com`,
+      ` `,
+      `Go to App`
+    ),
     text: `Thanks ${name} \n 
           Your account ${email} has been activated successfully.\n
-          You may now log in and begin using it.\n`
+          You may now log in and begin using it.\n`,
   };
   try {
-    await sgMail.send(mailGenerator );
+    await sgMail.send(mailGenerator);
   } catch (error) {
     return new HttpError(error.message, 500);
   }
 };
 
-module.exports = { accountVerifyEmail, accountActivatedEmail }
+module.exports = { accountVerifyEmail, accountActivatedEmail };
